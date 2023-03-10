5a0addc2c5a65c6d0edd59e647d6bd543058dd6a
Albert <albert@kinjalik.ru>
2023-03-10 04:05:19 +0300

    Lines sorted by line numbers

--- src/main/java/org/example/PPAVariableUsageCheckVisitor.kt
+++ src/main/java/org/example/PPAVariableUsageCheckVisitor.kt
@@ -2,17 +2,37 @@ package org.example
 
 import PPABaseVisitor
 import org.antlr.v4.runtime.tree.ParseTree
+import java.lang.Integer.parseInt
 
 class PPAVariableUsageCheckVisitor : PPABaseVisitor<Array<SyntaxError>>() {
     private val assignedVariables = mutableSetOf<String>()
+    private val varAssignLine = mutableMapOf<String, Int>()
     private var errors = arrayOf<SyntaxError>()
     private var errorSupression = mutableSetOf<Pair<Int, String>>() // { lineNumber, variableName}
+
+    override fun visitProgram(ctx: PPAParser.ProgramContext?): Array<SyntaxError> {
+        var res = super.visitProgram(ctx)
+
+        lines.sortBy { parseInt(it.getChild(0).text) }
+        lines.forEach { res = aggregateResult(res, super.visitLine(it)) }
+        return res
+    }
+
+    private val lines = mutableListOf<PPAParser.LineContext>()
+    override fun visitLine(ctx: PPAParser.LineContext?): Array<SyntaxError> {
+        if (ctx != null) {
+            lines += ctx
+        }
+        return errors
+    }
 
     override fun visitCommand(ctx: PPAParser.CommandContext?): Array<SyntaxError> {
         super.visitCommand(ctx)
 
         val name = ctx!!.assign_ident().VAR().text
+        val codeLine = ctx.start?.line ?: 0
         assignedVariables += name
+        varAssignLine[name] = codeLine
 
         super.visitCommand(ctx)
         return errors
@@ -22,7 +42,8 @@ class PPAVariableUsageCheckVisitor : PPABaseVisitor<Array<SyntaxError>>() {
         val name = ctx!!.VAR().text
         val codeLine = ctx.start?.line ?: 0
 
-        if (name !in assignedVariables && Pair(codeLine, name) !in errorSupression){
+        val isVarNotAssigned = name !in assignedVariables || varAssignLine[name] == codeLine
+        if (isVarNotAssigned && Pair(codeLine, name) !in errorSupression){
             errors += SyntaxError(String.format("Variable %s is not assigned", name), codeLine)
             errorSupression += Pair(codeLine, name)
         }
--- src/test/kotlin/VariableUsageCheckerTest.kt
+++ src/test/kotlin/VariableUsageCheckerTest.kt
@@ -74,4 +74,35 @@ internal class VariableUsageCheckerTest {
         val t = visitor.visit(parsedProgram.program())
         assertTrue { t.size == 1 }
     }
+
+    @Test
+    fun `use before assign with mixed line order`() {
+        val code = "10 y:=3;\n5 x:=y;"
+        val parsedProgram = parseProgram(code)
+        val visitor = PPAVariableUsageCheckVisitor()
+        val t = visitor.visit(parsedProgram.program())
+        print(t.size)
+        assertTrue { t.size != 0 }
+    }
+
+    @Test
+    fun `correct usage with mixed line order`() {
+        val code = "100 y:=x;\n1 x:=3;"
+        val parsedProgram = parseProgram(code)
+        val visitor = PPAVariableUsageCheckVisitor()
+        val t = visitor.visit(parsedProgram.program())
+        print(t.size)
+        assertTrue { t.size == 0 }
+    }
+
+    @Test
+    fun `variable assigned and used on the same line`() {
+        val code = "100 y:=sum(y,1,2);"
+        val parsedProgram = parseProgram(code)
+        val visitor = PPAVariableUsageCheckVisitor()
+        val t = visitor.visit(parsedProgram.program())
+        print(t.size)
+        assertTrue { t.size != 0 }
+    }
+
 }
